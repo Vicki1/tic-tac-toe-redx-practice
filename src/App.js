7c6bd6d29f3fa3ed2db66a49d0d7a7ef04d72569
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2017-09-11 10:58:19 -0600

    back-end done

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Tic-Tac-Toe</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <div className="row">
           <Square1/>
